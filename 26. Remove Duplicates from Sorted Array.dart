fe38d47f120005fb62e5d000cdb8f0b0232fd906
Niraj Phutane <nirajphutane.np@gmail.com>
2023-10-15 19:38:42 +0530

    Write a code for LeetCode: 26. Remove Duplicates from Sorted Array
    https://leetcode.com/problems/remove-duplicates-from-sorted-array/description

---
+++ 26. Remove Duplicates from Sorted Array.dart
@@ -0,0 +1,18 @@
+void main() {
+
+  List<int> nums = [0,0,1,1,1,2,2,3,3,4];
+  print(Solution().removeDuplicates(nums));
+}
+
+class Solution {
+  int removeDuplicates(List<int> nums) {
+    int j = 1;
+    for(int i = 1; i < nums.length; i++) {
+      if(nums[i-1] != nums[i]) {
+        nums[j] = nums[i];
+        j++;
+      }
+    }
+    return j;
+  }
+}
