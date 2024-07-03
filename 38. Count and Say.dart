7749b34d1a0256e28e1e34781a250ee6f3345d20
Niraj Phutane <nirajphutane.np@gmail.com>
2024-07-03 08:49:35 +0530

    Write a code for LeetCode: 38. Count and Say

--- 38. Count and Say.dart
+++ 38. Count and Say.dart
@@ -6,34 +6,63 @@ void main(List<String> args) {
   // 4 = "1211"
   // 5 = "111221"
   // 6 = "312211"
+  // 7 = "13112221"
 
-  print(Solution().countAndSay(6));
+  print(Solution().countAndSay(4));
 }
 
 class Solution {
   String countAndSay(int n) {
-    if(n == 1) {
-      return '1';
+    String res = '';
+    int i = 1;
+    while (i++ <= n) {
+      res = _count(res);
     }
-
-    return count(countAndSay(n-1));
+    return res;
   }
 
-  String count(String str) {
-    String result = '';
-
-    int frequency = 0, count = 0;
-    while(count < str.length) {
-      if(str[frequency] != str[count]) {
-        result += (count - frequency).toString();
-        result += str[count -1];
+  String _count(String str) {
+    if (str.isEmpty) {
+      return '1';
+    }
+    String countAndSay = '';
+    int count = 0, frequency = 0;
+    while (count < str.length) {
+      if (str[frequency] != str[count]) {
+        countAndSay += '${count-frequency}${str[frequency]}';
         frequency = count;
       }
       count++;
     }
-    result += (count - frequency).toString();
-    result += str[count -1];
-
-    return result;
+    countAndSay += '${count-frequency}${str[frequency]}';
+    return countAndSay;
   }
 }
+
+// class Solution {
+//   String countAndSay(int n) {
+//     if(n == 1) {
+//       return '1';
+//     }
+//
+//     return count(countAndSay(n-1));
+//   }
+//
+//   String count(String str) {
+//     String result = '';
+//
+//     int frequency = 0, count = 0;
+//     while(count < str.length) {
+//       if(str[frequency] != str[count]) {
+//         result += (count - frequency).toString();
+//         result += str[count -1];
+//         frequency = count;
+//       }
+//       count++;
+//     }
+//     result += (count - frequency).toString();
+//     result += str[count -1];
+//
+//     return result;
+//   }
+// }
